4627419cabeef67410300644962dd84b76333c2a
marco22st <80470097+marco22st@users.noreply.github.com>
2023-11-30 09:56:21 +0100

    Add comments

--- react-chatgpt-clone/server.js
+++ react-chatgpt-clone/server.js
@@ -1,38 +1,55 @@
+// Importiere benötigte Module und erstelle eine Express-Anwendung
 const PORT = 8000;
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const fetch = require("node-fetch"); // Stelle sicher, dass node-fetch installiert ist
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Lese den OpenAI API-Schlüssel aus der Umgebungsvariable
 const API_KEY = process.env.API_KEY;
 
-app.post('/completions', async (req, res) => {
+// Endpoint für die Anfrage an OpenAI GPT-3
+app.post("/completions", async (req, res) => {
+    // Optionen für die Anfrage an die OpenAI-API
     const options = {
-        method: 'POST',
+        method: "POST",
         headers: {
-            'Authorization': `Bearer ${API_KEY}`,
-            'Content-Type': 'application/json',
+            "Authorization": `Bearer ${API_KEY}`, // Setze den API-Schlüssel im Authorization-Header
+            "Content-Type": "application/json",
         },
         body: JSON.stringify({
-                model: 'gpt-3.5-turbo',
-                messages: [{ role: 'user', content: req.body.message }],
-                max_tokens: 100,
-            },
-        ),
+            max_tokens: 100,
+            messages: [{ content: req.body.message, role: "user" }],
+            model: "gpt-3.5-turbo"
+        }),
     };
+
     try {
-        const response = await fetch('https://api.openai.com/v1/chat/completions', options);
+        // Sende eine Anfrage an die OpenAI-API und warte auf die Antwort
+        const response = await fetch("https://api.openai.com/v1/chat/completions", options);
+
+        // Wandele die Antwort von der OpenAI-API in JSON um
         const data = await response.json();
+
+        // Sende die OpenAI-Antwort zurück an den Client
         res.send(data);
-        let historydata = {
-            'role': data.choices[0].message.role,
-            'message': data.choices[0].message.content,
-        };
+
+        // Der nachfolgende Code wird nicht ausgeführt, da er nach res.send() steht.
+        // Wenn du "historydata" verwenden möchtest, platziere diesen Code vor res.send().
+        // Zum Beispiel:
+        // let historydata = {
+        //     "role": data.choices[0].message.role,
+        //     "message": data.choices[0].message.content,
+        // };
     } catch (error) {
+        // Behandle Fehler, falls die Anfrage an die OpenAI-API fehlschlägt
         console.error(error);
     }
 });
 
-app.listen(PORT, () => console.log('Your server is running on PORT ' + PORT));
+// Starte den Server und höre auf Anfragen auf dem angegebenen Port
+app.listen(PORT, () => console.log("Your server is running on PORT " + PORT));
